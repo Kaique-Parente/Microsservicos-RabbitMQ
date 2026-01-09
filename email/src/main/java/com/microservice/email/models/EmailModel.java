62e14af894b923a9f8f8172e826810a7dc2ebeec
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-09 17:57:27 -0300

    [Microservices email ADD] EmailModel, StatusEmailEnum e EmailRepository

---
+++ email/src/main/java/com/microservice/email/models/EmailModel.java
@@ -0,0 +1,35 @@
+package com.microservice.email.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.microservice.email.enums.StatusEmail;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TB_EMAILS")
+@Getter
+@Setter
+public class EmailModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+    
+}
