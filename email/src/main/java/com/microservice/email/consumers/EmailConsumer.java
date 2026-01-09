c13acea8718af682c6a074c55a046274a3750f1f
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-09 17:57:57 -0300

    [Microservices email ADD] EmailConsumer e EmailService integração com SMTP e envio de email.

--- email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -1,18 +1,29 @@
 package com.microservice.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.microservice.email.dtos.EmailRecordDto;
+import com.microservice.email.models.EmailModel;
+import com.microservice.email.services.EmailService;
 
 @Component
 public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService){
+        this.emailService = emailService;
+    }
     
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
-        System.out.println(emailRecordDto.text());
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        
+        emailService.sendEmail(emailModel);
     }
     
 }
