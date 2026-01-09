c282c910b99e32e854daebe9c06329ebe708938a
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-09 01:54:00 -0300

    [Microservices email ADD] EmailConsumer iniciado

---
+++ email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -0,0 +1,18 @@
+package com.microservice.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.microservice.email.dtos.EmailRecordDto;
+
+@Component
+public class EmailConsumer {
+    
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
+        System.out.println(emailRecordDto.emailTo());
+        System.out.println(emailRecordDto.text());
+    }
+    
+}
