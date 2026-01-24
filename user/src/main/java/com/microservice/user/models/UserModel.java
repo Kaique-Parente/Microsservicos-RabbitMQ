04362825b294f0c060df3379cc0d594aad9ef7c1
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-24 00:53:56 -0300

    [Microservices user ADD] Builder no UserModel

--- user/src/main/java/com/microservice/user/models/UserModel.java
+++ user/src/main/java/com/microservice/user/models/UserModel.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UserModel implements Serializable{
     private static final long serialVersionUID = 1L;
 
